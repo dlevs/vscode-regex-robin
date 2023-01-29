c638fcb747bc54f9f26366c28f5aa7cf6721d40f
Daniel Levett <dlevett@hotmail.co.uk>
2023-01-29 13:13:33 +0000

    Improve parser and add more tests

--- src/util/stringUtils.test.ts
+++ src/util/stringUtils.test.ts
@@ -1,5 +1,9 @@
 import { describe, expect, test, vi } from "vitest";
-import { replaceMatches } from "./stringUtils";
+import {
+  compileExpression,
+  createExpressionsLexer,
+  replaceMatches,
+} from "./stringUtils";
 
 vi.mock("vscode");
 
@@ -58,38 +62,159 @@ describe("replaceMatches()", () => {
     ).toBe("0-1-10-21-100");
   });
 
-  // TODO: Extract this, and move to a different test / `description` block
   test("string transforms work", () => {
     expect(
-      replaceMatches("${0:toUpperCase():trim()}", [
-        { match: "  Hello world! " },
-      ])
+      // Testing a convoluted use with escape characters, chaining and regex arguments.
+      // Further tests for transforms are elsewhere.
+      replaceMatches(
+        "${0:toUpperCase():trim():replace('\\'', 'quote'):replace(/(\\{|\\}|:)/g, 'symbol.')}",
+        [{ match: "  Hello world! {}:' " }]
+      )
+    ).toBe("Hello world! symbol.symbol.symbol.quote");
+  });
+
+  // TODO: This one fails due to the regex looking for "${ ... }" getting confused with the closing curly bracket. Add it back.
+  // test("string transforms work", () => {
+  //   expect(
+  //     // Testing a convoluted use with escape characters, chaining and regex arguments.
+  //     // Further tests for transforms are elsewhere.
+  //     replaceMatches(
+  //       "${0:toUpperCase():trim():replace('\\'', 'quote'):replace(/(\\{|\\}|:)/g, 'symbol.')}",
+  //       [{ match: "  Hello world! {}:' " }]
+  //     )
+  //   ).toBe("HELLO WORLD! symbol.symbol.symbol.quote");
+  // });
+});
+
+describe("createExpressionsLexer()", () => {
+  const lexer = createExpressionsLexer();
+
+  test("parses matchIndexes", () => {
+    expect(lexer.input("2").tokens()).toMatchObject([
+      { type: "subject", value: { type: "matchIndex", key: 2 } },
+      { type: "EOF" },
+    ]);
+
+    expect(lexer.input("2:toUpperCase()").tokens()).toMatchObject([
+      { type: "subject", value: { type: "matchIndex", key: 2 } },
+      { type: "expressionSeparator" },
+      { type: "methodName", value: "toUpperCase" },
+      { type: "EOF" },
+    ]);
+  });
+
+  test("parses env variables", () => {
+    expect(lexer.input("env:NODE_ENV").tokens()).toMatchObject([
+      { type: "subject", value: { type: "envVariable", key: "NODE_ENV" } },
+      { type: "EOF" },
+    ]);
+
+    expect(lexer.input("env:NODE_ENV:toUpperCase()").tokens()).toMatchObject([
+      { type: "subject", value: { type: "envVariable", key: "NODE_ENV" } },
+      { type: "expressionSeparator" },
+      { type: "methodName", value: "toUpperCase" },
+      { type: "EOF" },
+    ]);
+  });
+
+  test("parses general variables", () => {
+    expect(lexer.input("foo").tokens()).toMatchObject([
+      { type: "subject", value: { type: "variable", key: "foo" } },
+      { type: "EOF" },
+    ]);
+    expect(lexer.input("lineNumber:toFixed(2)").tokens()).toMatchObject([
+      { type: "subject", value: { type: "variable", key: "lineNumber" } },
+      { type: "expressionSeparator" },
+      { type: "methodName", value: "toFixed" },
+      { type: "argument", value: 2 },
+      { type: "EOF" },
+    ]);
+  });
+
+  test("parses a complex chain of expressions as expected", () => {
+    const tokens = lexer
+      .input(
+        [
+          "2",
+          "toUpperCase()",
+          "slice(0, 5)",
+          'replace(/\\//, "\\"")',
+          "min(10, _)",
+          // The object keys have to be quoted with double quotes, unfortunately
+          'something(10, [true, { "foo-bar": 8 }], { "flag": false }, null, undefined, Infinity)',
+        ].join(":")
+      )
+      .tokens();
+
+    expect(tokens).toMatchObject([
+      { type: "subject", value: { type: "matchIndex", key: 2 } },
+      { type: "expressionSeparator" },
+      { type: "methodName", value: "toUpperCase" },
+      { type: "expressionSeparator" },
+      { type: "methodName", value: "slice" },
+      { type: "argument", value: 0 },
+      { type: "argument", value: 5 },
+      { type: "expressionSeparator" },
+      { type: "methodName", value: "replace" },
+      { type: "argument", value: /\// },
+      { type: "argument", value: '"' },
+      { type: "expressionSeparator" },
+      { type: "methodName", value: "min" },
+      { type: "argument", value: 10 },
+      { type: "argumentPlaceholder" },
+      { type: "expressionSeparator" },
+      { type: "methodName", value: "something" },
+      { type: "argument", value: 10 },
+      { type: "argument", value: [true, { "foo-bar": 8 }] },
+      { type: "argument", value: { flag: false } },
+      { type: "argument", value: null },
+      { type: "argument", value: undefined },
+      { type: "argument", value: Infinity },
+      { type: "EOF" },
+    ]);
+  });
+});
+
+describe("compileExpression()", () => {
+  test("string transforms work", () => {
+    expect(
+      compileExpression("0:toUpperCase():trim()")(["  Hello world! "])
     ).toBe("HELLO WORLD!");
-    expect(
-      replaceMatches("${0:slice(0, -1)}", [{ match: "Hello world!" }])
-    ).toBe("Hello world");
-    expect(
-      replaceMatches("${0:slice(0, +6.0):trim()}", [{ match: "Hello world!" }])
-    ).toBe("Hello");
-    expect(
-      replaceMatches("${0:replace(/\\|/, '_')}", [{ match: "A|B|C" }])
-    ).toBe("A_B|C");
-    expect(
-      replaceMatches("${0:replace(/\\|/g, '_')}", [{ match: "A|B|C" }])
-    ).toBe("A_B_C");
+    expect(compileExpression("0:slice(0, -1)")(["Hello world!"])).toBe(
+      "Hello world"
+    );
+    expect(compileExpression("0:slice(0, +6.0):trim()")(["Hello world!"])).toBe(
+      "Hello"
+    );
+    expect(compileExpression("0:replace(/\\|/, '_')")(["A|B|C"])).toBe("A_B|C");
+    expect(compileExpression("0:replace(/\\|/g, '_')")(["A|B|C"])).toBe(
+      "A_B_C"
+    );
   });
 
   test("number transforms work", () => {
-    expect(replaceMatches("${0:ceil()}", [{ match: "19.728" }])).toBe("20");
-    expect(
-      replaceMatches("${0:floor():toFixed(2)}", [{ match: "19.728" }])
-    ).toBe("19.00");
+    expect(compileExpression("0:ceil()")(["19.728"])).toBe("20");
+    expect(compileExpression("0:floor():toFixed(2)")(["19.728"])).toBe("19.00");
   });
 
   test("number transforms work with explicit variable positioning", () => {
-    expect(replaceMatches("${0:min(0, _, 1, 2)}", [{ match: "-10" }])).toBe(
-      "-10"
+    expect(compileExpression("0:min(0, _, 1, 2)")(["-10"])).toBe("-10");
+    expect(compileExpression("0:min(0, 1, 2)")(["-10"])).toBe("0");
+  });
+
+  test("escape characters are respected", () => {
+    expect(compileExpression("0:replace(/\\//, '\\'Hello\\'')")(["/"])).toBe(
+      "'Hello'"
     );
-    expect(replaceMatches("${0:min(0, 1, 2)}", [{ match: "-10" }])).toBe("0");
+  });
+
+  test("colons can be used in strings", () => {
+    expect(compileExpression("0:replace(/l/, ':')")(["ll"])).toBe(":l");
+  });
+
+  test("throws for malformed strings", () => {
+    expect(() => compileExpression("lineNumber:toFixed(2")).toThrow(
+      "Failed to parse expressions"
+    );
   });
 });
--- src/util/stringUtils.ts
+++ src/util/stringUtils.ts
@@ -1,67 +1,215 @@
-import * as vscode from "vscode";
-import Tokenizr from "tokenizr";
+// import * as vscode from "vscode";
+import Tokenizr, { ActionContext } from "tokenizr";
 
 // TODO: tidy
-const lexer = new Tokenizr();
 
-lexer.rule(/('|"|`)(.*?)\1/, (ctx, match) => {
-  ctx.accept("string", match[2]);
-});
+type Token =
+  | {
+      type: "subject";
+      value:
+        | { type: "envVariable"; key: string }
+        | { type: "variable"; key: string }
+        | { type: "matchIndex"; key: number };
+    }
+  | { type: "methodName"; value: string }
+  | { type: "argument"; value: unknown }
+  | { type: "argumentPlaceholder" }
+  | { type: "expressionSeparator" }
+  | { type: "EOF" };
 
-lexer.rule(/(?:\+|-)?\d+(?:\d|_|\.)*/, (ctx, match) => {
-  ctx.accept("number", Number(match));
-});
+function acceptToken(ctx: ActionContext, token: Token) {
+  ctx.accept(token.type, "value" in token ? token.value : undefined);
+}
 
-lexer.rule(/\/(.+?)\/([a-z]*)/, (ctx, match) => {
-  ctx.accept("regex", RegExp(match[1], match[2]));
-});
+const ARGUMENT_PLACEHOLDER = Symbol("placeholder");
 
-lexer.rule(/_/, (ctx, match) => {
-  ctx.accept("variable");
-});
+export function createExpressionsLexer() {
+  const lexer = new Tokenizr();
 
-lexer.rule(/^[a-zA-Z]+/, (ctx, match) => {
-  ctx.accept("methodName");
-});
+  lexer.rule(/^(?:env:)?.*?(?=(?::|$))/, (ctx, match) => {
+    // Shallow array
+    if (match[0].startsWith("env:")) {
+      acceptToken(ctx, {
+        type: "subject",
+        value: {
+          type: "envVariable",
+          key: match[0].replace("env:", ""),
+        },
+      });
+    } else if (/^\d+$/.test(match[0])) {
+      acceptToken(ctx, {
+        type: "subject",
+        value: {
+          type: "matchIndex",
+          key: Number(match[0]),
+        },
+      });
+    } else {
+      acceptToken(ctx, {
+        type: "subject",
+        value: {
+          type: "variable",
+          key: match[0],
+        },
+      });
+    }
+  });
 
-lexer.rule(/\(/, (ctx, match) => {
-  ctx.accept("openBracket");
-});
+  lexer.rule(/\[.*?\]/, (ctx, match) => {
+    // Shallow array
+    acceptToken(ctx, { type: "argument", value: JSON.parse(match[0]) });
+  });
 
-lexer.rule(/\)$/, (ctx, match) => {
-  ctx.accept("closeBracket");
-});
+  lexer.rule(/\{.*?\}/, (ctx, match) => {
+    // Shallow object
+    acceptToken(ctx, { type: "argument", value: JSON.parse(match[0]) });
+  });
 
-lexer.rule(/,/, (ctx, match) => {
-  ctx.accept("argumentSeparator");
-});
+  lexer.rule(/('|"|`)(.*?)(?<!\\)\1/, (ctx, match) => {
+    const value = match[2].replace(
+      new RegExp(`\\\\${match[1]}`, "g"),
+      match[1]
+    );
+    acceptToken(ctx, { type: "argument", value });
+  });
 
-lexer.rule(/\s*/, (ctx, match) => {
-  ctx.accept("whitespace");
-});
+  lexer.rule(/(?:\+|-)?\d+(?:\d|_|\.)*/, (ctx, match) => {
+    // Number
+    acceptToken(ctx, { type: "argument", value: Number(match) });
+  });
 
-// lexer.rule(/.*/, (ctx, match) => {
-//   ctx.accept("arguments");
-// });
+  lexer.rule(/\/(.+?)(?<!\\)\/([a-z]*)/, (ctx, match) => {
+    // Regex
+    acceptToken(ctx, { type: "argument", value: RegExp(match[1], match[2]) });
+  });
 
-function parseTransform(input: string) {
-  const tokens = lexer.input(input).tokens();
+  lexer.rule(/[a-zA-Z_$]+(?=\()/, (ctx, match) => {
+    acceptToken(ctx, { type: "methodName", value: match[0] });
+  });
 
-  return {
-    methodName: tokens[0]!.value,
-    args: tokens.flatMap((token) => {
-      if (
-        token.type === "string" ||
-        token.type === "number" ||
-        token.type === "regex" ||
-        token.type === "variable"
-      ) {
-        return token.value;
+  lexer.rule(/_/, (ctx, match) => {
+    acceptToken(ctx, { type: "argumentPlaceholder" });
+  });
+
+  lexer.rule(/(?:[a-zA-Z])+/, (ctx, match) => {
+    const value = match[0] === "null" ? null : globalThis[match[0]];
+    acceptToken(ctx, { type: "argument", value });
+  });
+
+  // Argument brackets and delimiters
+  lexer.rule(/[(),]/, (ctx, match) => {
+    ctx.ignore();
+  });
+
+  lexer.rule(/:/, (ctx, match) => {
+    acceptToken(ctx, { type: "expressionSeparator" });
+  });
+
+  lexer.rule(/\s*/, (ctx, match) => {
+    ctx.ignore();
+  });
+
+  return lexer;
+}
+const lexer = createExpressionsLexer();
+
+export function compileExpression(input: string) {
+  let getSubject: null | ((matches: string[]) => string) = null;
+  const transforms: {
+    method(...args: unknown[]): string | number;
+    args: unknown[];
+  }[] = [];
+
+  try {
+    // TODO: Compile these things once. Not every match
+    const tokens = lexer.input(input).tokens() as Token[];
+
+    for (const token of tokens) {
+      switch (token.type) {
+        case "subject": {
+          const { type, key } = token.value;
+          switch (type) {
+            case "matchIndex":
+              getSubject = (matches: string[]) => matches[key] ?? "";
+              break;
+            case "envVariable":
+              getSubject = () => process.env[key] ?? "";
+              break;
+            case "variable":
+              // TODO
+              break;
+          }
+          break;
+        }
+
+        case "methodName": {
+          const method = getMethodFromName(token.value);
+          if (!method) {
+            const errorMessage = `Unknown transform "${token.value}" ignored`;
+            // TODO: Put back. Or handle errors better higher up
+            // vscode.window.showErrorMessage(errorMessage);
+            throw new Error(errorMessage);
+          }
+          transforms.push({ method, args: [] });
+          break;
+        }
+
+        case "argument":
+        case "argumentPlaceholder": {
+          const lastTransform = transforms[transforms.length - 1];
+          if (!lastTransform) {
+            throw new Error("Found arguments with no method to pass them to");
+          }
+          lastTransform.args.push(
+            token.type === "argumentPlaceholder"
+              ? ARGUMENT_PLACEHOLDER
+              : token.value
+          );
+          break;
+        }
       }
+    }
+    if (getSubject == null) {
+      throw new Error("No subject provided");
+    }
+  } catch (err) {
+    console.error(err);
+    throw new SyntaxError(`Failed to parse expressions. ${err.message}`);
+  }
 
-      return [];
-    }),
+  return function processMatches(matches: string[]) {
+    let output: string | number = getSubject!(matches);
+
+    for (const transform of transforms) {
+      output = transform.method.apply(output, transform.args);
+    }
+
+    return String(output);
   };
+}
+
+function getMethodFromName(methodName: string): void | (() => string | number) {
+  if (methodName in String.prototype) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return String.prototype[methodName];
+  } else if (methodName in Number.prototype) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return Number.prototype[methodName];
+  } else if (methodName in Math) {
+    return function mathFunction(this: string, ...args: unknown[]) {
+      const mathArguments =
+        args.length === 0
+          ? [Number(this)]
+          : args.map((arg) =>
+              arg === ARGUMENT_PLACEHOLDER ? Number(this) : Number(arg)
+            );
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      return Math[methodName](...mathArguments);
+    };
+  }
 }
 
 /**
@@ -87,41 +235,11 @@ export function replaceMatches(
           : typeof group2 === "string"
           ? group2 // E.g. ${1}
           : "";
-      const [variableName, ...transforms] = expression.split(":");
 
-      if (!variableName) {
-        return "";
-      }
-
-      const isNumber = /^\d+$/.test(variableName);
-
-      let output: string | number = isNumber
-        ? matchGroups[Number(variableName)]?.match ?? ""
-        : "";
-
-      for (const transform of transforms) {
-        // TODO: Error handle
-        const { methodName, args } = parseTransform(transform);
-
-        if (methodName in String.prototype) {
-          output = String(output)[methodName as any](...args);
-        } else if (methodName in Number.prototype) {
-          output = Number(output)[methodName as any](...args);
-        } else if (methodName in Math) {
-          // TODO: Add tests for this
-          const mathArguments =
-            args.length === 0
-              ? [Number(output)]
-              : args.map((arg) => (arg === "_" ? Number(output) : Number(arg)));
-          output = Math[methodName](...mathArguments);
-        } else {
-          vscode.window.showErrorMessage(
-            `Unknown transform "${transform}" ignored`
-          );
-        }
-      }
-
-      return String(output);
+      // TODO: Move this. The expressions should be compiled once, not for every match. It's here for now just for testing.
+      return compileExpression(expression)(
+        matchGroups.map((group) => group?.match ?? "")
+      );
     })
     .replace(/\\\$/g, "$");
 }
