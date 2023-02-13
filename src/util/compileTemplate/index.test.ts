cf40e3aa40be4844d32e8e198da2ee2247b4bae7
Daniel Levett <dlevett@hotmail.co.uk>
2023-02-13 23:22:39 +0000

    Simplify

--- src/util/compileTemplate/index.test.ts
+++ src/util/compileTemplate/index.test.ts
@@ -1,39 +1,28 @@
 import { describe, expect, test } from "vitest";
-import { compileTemplate } from "./";
+import { compileTemplate } from ".";
 
+// TODO: Revisit this file. Some of the tests don't really make sense now that we're just using raw expressions instead of creating a language
 describe("compileTemplate()", () => {
   test("string transforms work", () => {
     expect(
-      compileTemplate("${0:toUpperCase():trim()}")(["  Hello world! "])
+      compileTemplate("${$0.toUpperCase().trim()}")(["  Hello world! "])
     ).toBe("HELLO WORLD!");
-    expect(compileTemplate("${0:slice(0, -1)}")(["Hello world!"])).toBe(
+    expect(compileTemplate("${$0.slice(0, -1)}")(["Hello world!"])).toBe(
       "Hello world"
     );
-    expect(compileTemplate("${0:slice(0, 6.0):trim()}")(["Hello world!"])).toBe(
-      "Hello"
-    );
-    expect(compileTemplate("${0:replace(/\\|/, '_')}")(["A|B|C"])).toBe(
+    expect(
+      compileTemplate("${$0.slice(0, 6.0).trim()}")(["Hello world!"])
+    ).toBe("Hello");
+    expect(compileTemplate("${$0.replace(/\\|/, '_')}")(["A|B|C"])).toBe(
       "A_B|C"
     );
-    expect(compileTemplate("${0:replace(/\\|/g, '_')}")(["A|B|C"])).toBe(
+    expect(compileTemplate("${$0.replace(/\\|/g, '_')}")(["A|B|C"])).toBe(
       "A_B_C"
     );
   });
 
-  test("number transforms work", () => {
-    expect(compileTemplate("${0:ceil()}")(["19.728"])).toBe("20");
-    expect(compileTemplate("${0:floor():toFixed(2)}")(["19.728"])).toBe(
-      "19.00"
-    );
-  });
-
-  test("number transforms work with explicit variable positioning", () => {
-    expect(compileTemplate("${0:min(0, _, 1, 2)}")(["-10"])).toBe("-10");
-    expect(compileTemplate("${0:min(0, 1, 2)}")(["-10"])).toBe("0");
-  });
-
   test("escape characters are respected", () => {
-    expect(compileTemplate("${0:replace(/\\//, '\\'Hello\\'')}")(["/"])).toBe(
+    expect(compileTemplate("${$0.replace(/\\//, '\\'Hello\\'')}")(["/"])).toBe(
       "'Hello'"
     );
   });
@@ -45,7 +34,7 @@ describe("compileTemplate()", () => {
   });
 
   test("colons can be used in strings", () => {
-    expect(compileTemplate("${0:replace(/l/, ':')}")(["ll"])).toBe(":l");
+    expect(compileTemplate("${$0.replace(/l/, ':')}")(["ll"])).toBe(":l");
   });
 
   // TODO
--- src/util/compileTemplate/index.ts
+++ src/util/compileTemplate/index.ts
@@ -1,15 +1,9 @@
-import nearley from "nearley";
-import rawGrammar, { type TemplateNode } from "./grammar";
+import { parseTemplate, TemplateNode } from "./parseTemplate";
 
 type MatchProcessor = (matches: string[]) => string;
 
-const grammar = nearley.Grammar.fromCompiled(rawGrammar);
-const ARGUMENT_PLACEHOLDER = Symbol("placeholder");
-
 export function compileTemplate(template: string) {
-  const parser = new nearley.Parser(grammar);
-  parser.feed(template);
-  const nodes: TemplateNode[] = parser.results[0];
+  const nodes = parseTemplate(template);
   const matchProcessors = nodes.map(processNode);
 
   return function populateTemplate(matches: string[]) {
@@ -21,87 +15,40 @@ function processNode(node: TemplateNode): MatchProcessor {
   switch (node.type) {
     case "text":
       return () => node.value;
-    case "template":
-      return createTemplateMatcher(node);
+    case "expression": {
+      const template = Function(
+        "$0",
+        "$1",
+        "$2",
+        "$3",
+        "$4",
+        "$5",
+        "$6",
+        "$7",
+        "$8",
+        "$9",
+        "lineNumber",
+        `return ${node.value}`
+      );
+      console.log(node.value);
+      return (matches) => {
+        const args = [
+          matches[0] || "",
+          matches[1] || "",
+          matches[2] || "",
+          matches[3] || "",
+          matches[4] || "",
+          matches[5] || "",
+          matches[6] || "",
+          matches[7] || "",
+          matches[8] || "",
+          matches[9] || "",
+          10, // TODO:
+        ];
+        return template(...args);
+      };
+    }
     default:
-      // @ts-expect-error - exhaustive switch
       throw new SyntaxError("Unexpected node type: " + node.type);
   }
-}
-
-function createTemplateMatcher(
-  node: Extract<TemplateNode, { type: "template" }>
-): MatchProcessor {
-  const { value } = node;
-  const subjectType = /\d+/.test(value)
-    ? "matchIndex"
-    : /^env:/.test(value)
-    ? "envVariable"
-    : "variable";
-
-  const getSubject: MatchProcessor =
-    subjectType === "matchIndex"
-      ? (matches) => matches[Number(value)] ?? ""
-      : subjectType === "envVariable"
-      ? () => process.env[value] ?? ""
-      : () => "TODO";
-
-  const transforms = node.transforms.map((transform) => ({
-    ...transform,
-    method: getMethodFromName(transform.methodName),
-  }));
-
-  return function processMatches(matches) {
-    let output: string | number = getSubject(matches);
-
-    for (const transform of transforms) {
-      const args = transform.args.map((arg: any) => {
-        // TODO:Allow like ${process.env.NODE_ENV:replace(/dev/, 'development')}?
-        if (
-          arg != null &&
-          typeof arg === "object" &&
-          "type" in arg &&
-          arg.type === "identifier"
-        ) {
-          if (arg.value === "_") {
-            return ARGUMENT_PLACEHOLDER;
-          }
-          return globalThis[arg.value];
-        }
-
-        return arg;
-      });
-      output = transform.method.apply(output, args);
-    }
-
-    return String(output);
-  };
-}
-
-function getMethodFromName(
-  methodName: string
-): (...args: unknown[]) => string | number {
-  if (methodName in String.prototype) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    return String.prototype[methodName];
-  } else if (methodName in Number.prototype) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    return Number.prototype[methodName];
-  } else if (methodName in Math) {
-    return function mathFunction(this: string, ...args: unknown[]) {
-      const mathArguments =
-        args.length === 0
-          ? [Number(this)]
-          : args.map((arg) =>
-              arg === ARGUMENT_PLACEHOLDER ? Number(this) : Number(arg)
-            );
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      return Math[methodName](...mathArguments);
-    };
-  }
-
-  throw new SyntaxError("Unknown method: " + methodName);
 }
