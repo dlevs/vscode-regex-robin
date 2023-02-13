c2cd576f611e95df818c218a8f0f5f43acf4d36f
Daniel Levett <dlevett@hotmail.co.uk>
2023-02-13 23:30:26 +0000

    Combine related functions into one file

--- src/util/compileTemplate.ts
+++ src/util/compileTemplate.ts
@@ -1,12 +1,76 @@
 type State = "text" | "expression";
-const delimiters = ["'", '"', "`", "/"] as const;
+type MatchProcessor = (matches: string[]) => string;
 
-export interface TemplateNode {
+interface TemplateNode {
   type: State;
   value: string;
 }
 
+export function compileTemplate(template: string) {
+  const nodes = parseTemplate(template);
+  const matchProcessors = nodes.map(processNode);
+
+  return function populateTemplate(matches: string[]) {
+    return matchProcessors.map((processor) => processor(matches)).join("");
+  };
+}
+
+function processNode(node: TemplateNode): MatchProcessor {
+  switch (node.type) {
+    case "text":
+      return () => node.value;
+    case "expression": {
+      const template = Function(
+        "$0",
+        "$1",
+        "$2",
+        "$3",
+        "$4",
+        "$5",
+        "$6",
+        "$7",
+        "$8",
+        "$9",
+        "lineNumber",
+        `return ${node.value}`
+      );
+      console.log(node.value);
+      return (matches) => {
+        const args = [
+          matches[0] || "",
+          matches[1] || "",
+          matches[2] || "",
+          matches[3] || "",
+          matches[4] || "",
+          matches[5] || "",
+          matches[6] || "",
+          matches[7] || "",
+          matches[8] || "",
+          matches[9] || "",
+          10, // TODO:
+        ];
+        return template(...args);
+      };
+    }
+    default:
+      throw new SyntaxError("Unexpected node type: " + node.type);
+  }
+}
+
+const delimiters = ["'", '"', "`", "/"] as const;
+
 /**
+ * Parse a string containing template expressions into an array of nodes.
+ *
+ * For example, in the string "Hello ${world}, my name is $1", the following
+ * would be identified as expressions:
+ * - "${world}"
+ * - "$1"
+ *
+ * Most of the logic here is to handle nested expressions, objects and strings
+ * so that we don't terminate the expression too early in templates like:
+ * - "Foo ${'{bar}'}"
+ *
  *
  */
 export function parseTemplate(template: string) {
